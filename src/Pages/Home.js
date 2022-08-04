450033178c6ab8bd8971a1a5c38e858aaf713074
Sojo C Johny <sojocjohny37@gmail.com>
2022-08-04 22:03:34 +0530

    Added a section to view options and Banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from '../Components/Header/Header';
-// import Banner from '../Components/Banner/Banner';
+import Banner from '../Components/Banner/Banner';
 
 // import Posts from '../Components/Posts/Posts';
 // import Footer from '../Components/Footer/Footer';
@@ -9,7 +9,8 @@ import Header from '../Components/Header/Header';
 function Home(props) {
   return (
     <div className="homeParentDiv">
-      <Header />
+      <Header/>
+      <Banner/>
     </div>
   );
 }
